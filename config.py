5f9fd35a643f126d141dd09ff2d5d64e9c3bcc3a
Peyman Tahghighi <tahghighipeyman@gmail.com>
2023-10-06 19:38:47 -0600

    Minor fixes

--- config.py
+++ config.py
@@ -1,12 +1,12 @@
 import torch
 DEVICE = 'cuda' if torch.cuda.is_available() else 'cpu';
 hyperparameters = {
-    'batch_size' : 1,
+    'batch_size' : 2,
     'crop_size_w': 128,
     'crop_size_h': 128,
     'crop_size_d':64,
     'learning_rate':1e-3,
-    'sample_per_mri':1,
+    'sample_per_mri':4,
     'deterministic':False,
     'virtual_batch_size':1
 }
